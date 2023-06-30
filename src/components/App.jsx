82a5f005263463f3da1b0b376655b3cd4223e0c9
yuliasiromakha <93142108+yuliasiromakha@users.noreply.github.com>
2023-06-30 18:01:09 +0300

    errors

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,29 @@
-export const App = () => {
+// 73afe0ea7b3891ac6681393403683c27
+
+import Movies from '../pages/Movies';
+import Home from '../pages/Home';
+import { Route, Routes, NavLink } from 'react-router-dom';
+
+const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <nav>
+        <ul>
+          <li>
+            <NavLink to="/">Home</NavLink>
+          </li>
+          <li>
+            <NavLink to="/movies">Movies</NavLink>
+          </li>
+        </ul>
+      </nav>
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/movies" element={<Movies />} />
+      </Routes>
     </div>
   );
-};
+}
+
+export default App;
---
+++ src/pages/Home.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Home = () => {
+    return (
+      <>
+        <h2>Trending today</h2>
+        <ul>
+          <li>Movie 1</li>
+          <li>Movie 2</li>
+          <li>Movie 3</li>
+        </ul>
+      </>
+    );
+  };
+  
+
+export default Home;
